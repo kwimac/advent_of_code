26819580fc1c4a0763fa1e852fd45b3b6e9512a9
Maciej <maciej.kwiatkowski92@gmail.com>
2024-12-08 12:48:12 +0100

    Day 7 year 2024 fix

--- 2024/day_07.py
+++ 2024/day_07.py
@@ -1,29 +1,64 @@
-def _process_line(line: str) -> int:
+from pyparsing import deque
+
+
+def _parse_line(line: str) -> tuple[int, list[str]]:
     result, values = line.split(":")
     result = int(result.strip())
-    values = list(map(int, values.split()))
+    values = values.split()
+    return result, values
 
-    n_bits = len(values)
-    for ii in range(2**n_bits-1):
-        value = values[0]
-        for val, char in zip(values[1:], f"{ii:b}".zfill(n_bits)):
-            value = value + val if char == "0" else value * val
-        if value == result:
-            return result
+def _process_line(result: int, values: list[str]) -> int:
+    operators = {
+        '+': lambda x, y: x+int(y),
+        '*': lambda x, y: x*int(y),
+    }
+    n = len(values)
+    val = int(values[0])
+    states = deque([(val, 1, '+'), (val, 1, '*')])
+    while states:
+        val, ii, operator = states.popleft()
+        if ii<n:
+            val = operators[operator](val, values[ii])
+            if val == result:
+                return result
+            if val > result:
+                continue
+            states.extend((val, ii+1, operator) for operator in operators)
     return 0
 
 
 def task_1(data: list[str]) -> int:
-    return sum(_process_line(line) for line in data)
+    return sum(_process_line(*_parse_line(line)) for line in data)
+
+
+def _process_line_2(result: int, values: list[str]) -> int:
+    operators = {
+        '+': lambda x, y: int(x)+int(y),
+        '*': lambda x, y: int(x)*int(y),
+        '||': lambda x, y: int(x+y),
+    }
+    n = len(values)
+    val = values[0]
+    states = deque([(val, 1, '+'), (val, 1, '*'), (val, 1, '||')])
+    while states:
+        val, ii, operator = states.popleft()
+        if ii<n:
+            val = operators[operator](val, values[ii])
+            if val == result:
+                return result
+            if val > result:
+                continue
+            states.extend((str(val), ii+1, operator) for operator in operators)
+    return 0
 
 
 def task_2(data: list[str]) -> int:
-    ...
+    return sum(_process_line_2(*_parse_line(line)) for line in data)
 
 
 if __name__ == "__main__":
     # with open("2024/data/example_07.txt", "r", encoding="utf-8") as _file:
     with open("2024/data/input_07.txt", "r", encoding="utf-8") as _file:
         data_ = _file.readlines()
-        print(task_1(data_))
-        # print(task_2(data_))
+        # print(task_1(data_))
+        print(task_2(data_))
